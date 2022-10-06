a4dc1429f225caecbfb6b13faa7a5c66426eb072
Deebendu <>
2022-10-06 23:05:58 +0530

    Updated

---
+++ spring-boot-common-bom/src/test/java/in/zestic/springboot/common/SpringBootCommonBomApplicationTests.java
@@ -0,0 +1,13 @@
+package in.zestic.springboot.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCommonBomApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
